42fa81ce12dfeead4eebc2106979752db7758c29
fp <huang.fp@gmail.com>
2022-02-13 16:27:51 -0600

    get cors working

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+let socket;
 
 const Chat = () => {
+    const location = useLocation();
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT, {
+            withCredentials: true,
+            // extraHeaders: {
+            //     'my-custom-header': 'abcd'
+            // }
+        });
+        setName(name);
+        setRoom(room);
+
+        console.log(socket);
+    }, [location.search]);
+
     return (
-    <h1>Chat</h1>
+        <h1>Chat</h1>
     );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 const router = require('./router');
@@ -8,13 +9,27 @@ const { isPromise } = require('util/types');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods:["GET", "POST"],
+        // allowedHeaders: ["my-custom-header"],
+        credentials: true
+    }
+});
+
 io.on('connection', socket => {
     console.log('We have a new connection!');
     socket.on('disconnect', () => {
         console.log('User had left!');
     });
 })
+
+// var corsOptions = {
+//     origin: '*',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+//   }
+//   app.use(cors(corsOptions));
 
 app.use(router);
 
